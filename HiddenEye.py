bbcdb3f60e5d280d6e105bfd11a1a921e293d34a
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2021-02-01 13:25:06 +0800

    Update HiddenEye.py

--- HiddenEye.py
+++ HiddenEye.py
@@ -1,5 +1,5 @@
 #
-#    HiddenEye  Copyright (C) 2020  DarkSec https://dark-sec-official.com
+#    HiddenEye  Copyright (C) 2020  DarkSec https://dark-sec-official.net
 #    This program comes with ABSOLUTELY NO WARRANTY; for details read LICENSE.
 #    This is free software, and you are welcome to redistribute it
 #    under certain conditions; you can read LICENSE for details.
