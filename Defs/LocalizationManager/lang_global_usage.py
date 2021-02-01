98f8130485359107b4da3aaad14b2061c56da0e1
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2021-02-01 13:36:22 +0800

    Update lang_global_usage.py

--- Defs/LocalizationManager/lang_global_usage.py
+++ Defs/LocalizationManager/lang_global_usage.py
@@ -16,17 +16,17 @@ hidden_eye_logo = """
  {1} ██   ██ ██ ██████   ██████   ███████ ██   ███  {2}███████    ██    ███████ {0}""".format(
     default_palette[4], default_palette[2], default_palette[0])
 
-input_line = "\n{0}HiddenEye >>>  {1}".format(default_palette[0],
+input_line = "\n{0}MR.404 >>>  {1}".format(default_palette[0],
                                               default_palette[2])
-official_website_link = "{0}https://dark-sec-official.com".format(
+My channel = "{0}https://youtube.com/channel/UC0IpDdp5KzL6RfX1RpUxU7Q".format(
     default_palette[0])
-by_darksec = "{0}** BY:DARKSEC **".format(default_palette[0])
+by_darksec = "{0}** BY:MR.404 **".format(default_palette[0])
 line_of_dots = "{0}...............................".format(default_palette[0])
 small_logo = """{1}
          _  _ . ___  ___  ___ _  _  {0}___ _  _ ___{1}
          |__| | ]  | ]  | |__ |\ |  {0}|__ \__/ |__{1}
          |  | | ]__| ]__| |__ | \|  {0}|__  ||  |__{1}
-         {1}http://github.com/darksecdevelopers
-         {0}** BY: {1}DARKSEC {0}**""".format(default_palette[0],
+         {1}http://github.com/MR404cyber
+         {0}** BY: {1}MR.404/RAIHAN {0}**""".format(default_palette[0],
                                               default_palette[2])
 invalid_option = "Please choose a valid option."
